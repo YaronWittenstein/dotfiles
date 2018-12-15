777156e615e410c0970faf289bd7d60be40b9f13
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-15 23:04:37 +0200

    init.el

--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,17 @@
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+(add-hook 'rust-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
 
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 (setq create-lockfiles nil)
+
+(custom-set-variables
+ '(package-selected-packages (quote (cargo racer go-mode evil))))
+(custom-set-faces
+ )
